6dd3f712add95e70e296931c0a16b8eda18df6ad
Igor Kudlo <igoreklida@gmail.com>
2021-01-04 17:58:57 +0300

    reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello Redux')
+const initialState = 0;
+
+const reducer = (state, action) => {
+
+    if (state === undefined) {
+        return 0;
+    }
+
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+
+        default:
+            return state;
+    }
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, { type: 'INC' });
+state = reducer(state, { type: 'INC' });
+
+console.log(state)
