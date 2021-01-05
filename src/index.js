62faef87433f16e061bbc0861010b7e3d47d112e
IgorKudlo <igoreklida@gmail.com>
2021-01-05 23:05:44 +0300

    bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 import reducer from "./reducer";
 import { inc, dec, rnd } from "./actions";
 
 const store = createStore(reducer);
+const { dispatch } = store;
+
+const { incDispatch, decDispatch, rndDispatch } = bindActionCreators({
+  incDispatch: inc,
+  decDispatch: dec,
+  rndDispatch: rnd,
+}, dispatch);
 
 document
     .getElementById('inc')
-    .addEventListener('click', () => {
-        store.dispatch(inc());
-    });
+    .addEventListener('click', incDispatch);
 
 document
   .getElementById('dec')
-  .addEventListener('click', () => {
-      store.dispatch(dec());
-  });
+  .addEventListener('click', decDispatch);
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random()*10);
-    store.dispatch(rnd(payload));
+    rndDispatch(payload);
   });
 
 const update = () => {
